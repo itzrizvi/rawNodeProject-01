d570df239d6785c2eae148b9d29134d097b62717
itzrizvi <shahriar.me.work@gmail.com>
2022-01-06 12:34:33 +0600

    Updated

--- index.js
+++ index.js
@@ -10,15 +10,30 @@ const data = require("./lib/data");
 // App main
 const app = {};
 
-// Testing File System ---> ###
-data.create(
-  "test",
-  "newFile",
-  { name: "Bangladesh", lang: "Bangla" },
-  (err) => {
-    console.log(`Error was ${err}`);
-  }
-);
+// Testing File System Starts---> ###
+
+// data.create(
+//   "test",
+//   "newFile",
+//   { name: "Bangladesh", lang: "Bangla" },
+//   (err) => {
+//     console.log(`Error was ${err}`);
+//   }
+// );
+
+// data.read("test", "newFile", (err, data) => {
+//   console.log(err, data);
+// });
+
+// data.update("test", "newFile", { name: "England", lang: "English" }, (err) => {
+//   console.log(err);
+// });
+
+// data.delete("test", "newFile", (err) => {
+//   console.log(err);
+// });
+
+// Testing File System Ends  ---> ###
 
 // Cerate server
 app.createServer = () => {
